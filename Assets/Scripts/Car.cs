68d045725b03b58a5f0d162d648643d5322fce70
Matteo Beltrame <bltmtt@gmail.com>
2020-02-17 02:14:08 +0100

    initial commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public NeuralNet neuralNet;
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    private Rigidbody rigidbody;
+
+    public float throttle, steering;
+    void Start()
+    {
+        neuralNet = gameObject.AddComponent<NeuralNet>();
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        //if (collider.transform.childCount == 0)
+        //{
+        //    return;
+        //}
+
+        //Transform visualWheel = collider.transform.GetChild(0);
+
+        //Vector3 position;
+        //Quaternion rotation;
+        //collider.GetWorldPose(out position, out rotation);
+
+        //visualWheel.transform.position = position;
+        //visualWheel.transform.rotation = rotation;
+    }
+
+    void FixedUpdate()
+    {
+        float motor = throttle * Input.GetAxis("Vertical");
+        float turn = steering * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = turn;
+                axleInfo.rightWheel.steerAngle = turn;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
+        }
+
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
---
+++ Assets/Scripts/NeuralNet.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuralNet : MonoBehaviour
+{
+    public int inputsCount = 5;
+    public int outputsCount = 3;
+	public int hiddenLayerCount = 2;
+	public int neuronsPerHiddenLayer = 5;
+
+	[SerializeField] public int directionsCount;
+
+	float[] inputs;
+    float[] outputs;
+
+    float[][] i_h0Weights;
+    float[][] h0_h1Weights;
+    float[][] h1_oWeights;
+
+
+	void Awake()
+    {
+		directionsCount = inputsCount - 1;
+		inputs = new float[inputsCount];
+        outputs = new float[outputsCount];
+
+		i_h0Weights = new float[inputsCount][];
+		for(int i = 0; i < inputsCount; i++)
+		{
+			i_h0Weights[i] = new float[neuronsPerHiddenLayer];
+			for(int j = 0; j < neuronsPerHiddenLayer; j++)
+			{
+				i_h0Weights[i][j] = Random.Range(0F, 1F);
+			}
+		}
+
+		h0_h1Weights = new float[neuronsPerHiddenLayer][];
+		for (int i = 0; i < neuronsPerHiddenLayer; i++)
+		{
+			h0_h1Weights[i] = new float[neuronsPerHiddenLayer];
+			for (int j = 0; j < neuronsPerHiddenLayer; j++)
+			{
+				h0_h1Weights[i][j] = Random.Range(0F, 1F);
+			}
+		}
+
+		h1_oWeights = new float[neuronsPerHiddenLayer][];
+		for (int i = 0; i < neuronsPerHiddenLayer; i++)
+		{
+			h1_oWeights[i] = new float[outputsCount];
+			for (int j = 0; j < outputsCount; j++)
+			{
+				h1_oWeights[i][j] = Random.Range(0F, 1F);
+			}
+		}
+	}
+
+	private float[][] MatricesMultiplication(float[][] a, int ra, int ca, float[][] b, int rb, int cb)
+	{
+		if (ra != cb)
+		{
+			return null;
+		}
+
+		float[][] matrix = new float[ra][];
+		for (int i = 0; i < ra; i++)
+		{
+			matrix[i] = new float[cb];
+		}
+
+		float sum = 0;
+		for (int i = 0; i < ra; i++)
+		{
+			for (int j = 0; j < cb; j++)
+			{
+				for (int k = 0; k < rb; k++)
+				{
+					sum = sum + a[i][k] * b[k][j];
+				}
+
+				matrix[i][j] = sum;
+				sum = 0;
+			}
+		}
+		return matrix;
+	}
+}
